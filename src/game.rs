1ae0f3122f7e790e98222d95c760848bffc26b32
Joe Einertson <jeinerts@umm>
2018-09-03 23:52:31 -0500

    WIP

--- src/game.rs
+++ src/game.rs
@@ -17,6 +17,12 @@ struct GridPosition {
     z: i8,
 }
 
+struct GridDirection {
+    x: i8,
+    y: i8,
+    z: i8,
+}
+
 struct GridSpace {
     atom: Option<Atom>,
     selected: bool,
@@ -25,6 +31,50 @@ struct GridSpace {
 struct Grid {
     radius: u8,
     spaces: HashMap<GridPosition, GridSpace>,
+}
+
+static DIRECTIONS: [&GridDirection; 6] = [
+    &GridDirection { x: 1, y: -1, z: 0 },
+    &GridDirection { x: 1, y: 0, z: -1 },
+    &GridDirection { x: 0, y: 1, z: -1 },
+    &GridDirection { x: -1, y: 1, z: 0 },
+    &GridDirection { x: -1, y: 0, z: 1 },
+    &GridDirection { x: 0, y: -1, z: 1 },
+];
+
+/*lazy_static! {
+
+}*/
+
+fn grid_add(pos: &GridPosition, dir: &GridDirection) -> GridPosition {
+    GridPosition {
+        x: pos.x + dir.x,
+        y: pos.y + dir.y,
+        z: pos.z + dir.z,
+    }
+}
+
+fn grid_scale(dir: &GridDirection, radius: i8) -> GridDirection {
+    GridDirection {
+        x: dir.x * radius,
+        y: dir.y * radius,
+        z: dir.z * radius,
+    }
+}
+
+fn grid_ring(center: GridPosition, radius: i8) -> Vec<GridPosition> {
+    let mut tiles: Vec<GridPosition> = Vec::new();
+
+    let mut current = grid_add(&center, &grid_scale(DIRECTIONS[4], radius));
+
+    for i in 0..6 {
+        for j in 0..radius {
+            tiles.push(current);
+            current = grid_add(&current, DIRECTIONS[i]);
+        }
+    }
+
+    tiles
 }
 
 impl Grid {
@@ -61,8 +111,6 @@ impl Grid {
                     selected: false,
                 },
             );
-
-            x += deltas[i];
 
             // x += deltas[i]
             // y += deltas[i + 2]
